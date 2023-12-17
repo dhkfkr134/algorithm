b31e62715ea7940344731709ae5ebba7d8b395d7
dhkfkr134 <dhkfkr134@naver.com>
2023-12-17 19:43:13 +0900

    2darray problem java

--- java/_2darray/Ex03.java
+++ java/_2darray/Ex03.java
@@ -9,5 +9,22 @@ import java.util.Scanner;
 public class Ex03 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    String[] arr = new String[5];
+    char[][] trr = new char[15][5];
+    for (int i = 0; i < 5; i++) {
+      arr[i] = scanner.nextLine();
+      }
+    for (int j = 0; j < 5; j++) {
+      for (int k = 0; k < arr[j].length(); k++) {
+        System.out.printf("%d, %d, %d\n",j,k,arr[j].length());
+        trr[k][j] = arr[j].charAt(k);
+      }
+    }
+    for(int i = 0; i < 5 ; i++) {
+      for (int j =0 ; j < trr[i].length; j++) {
+        System.out.printf("%c ", trr[i][j]);
+      }
+      System.out.println("");
+    }
   }
 }
