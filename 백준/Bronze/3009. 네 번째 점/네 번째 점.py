0190134a74031122d63eff4fbf16ff874ad23fce
김정원 <dhkfkr134@naver.com>
2024-01-03 09:38:18 +0900

    [Bronze III] Title: 네 번째 점, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,24 +1,21 @@
 # 1. 뭔가 간단한 방법이 생각 안남
 # 2. x와 y를 따로 비교한다.
 # 3. x값중 한개만 있으면 그 값이 찾으려는 점의 x, y도 마찬가지
-import sys
-arr = list(list(map(int, sys.stdin.readline().split())) for _ in range(3))
-tempx = arr[0][0]
-tempy = arr[0][1]
-cntx = 0
-cnty = 0
-x = 0
-y = 0
-for i in range(2):
-    if tempx != arr[i+1][0]:
-        x = arr[i+1][0]
-        cntx += 1
-        if cntx == 2:
-            x = tempx
-    if tempy != arr[i+1][1]:
-        y = arr[i+1][1]
-        cnty += 1
-        if cnty == 2:
-            y = tempy
 
-print(f'{x} {y}')
+# count함수를 알면 코드가 간결해진다.
+# 아이디어는 같음
+X = []
+Y = []
+
+for i in range(3):
+    x, y = map(int, input().split())
+    X.append(x)
+    Y.append(y)
+
+for i in range(3):
+    if X.count(X[i]) == 1:
+        x = X[i]
+    if Y.count(Y[i]) == 1:
+        y = Y[i]
+
+print(x, y)
