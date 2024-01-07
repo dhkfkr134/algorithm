b08b4b56e839bf9eff3d1fecd946a52ff8bf2dc8
김정원 <dhkfkr134@naver.com>
2024-01-07 17:00:50 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.py
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.py
@@ -1,5 +1,4 @@
+# 등차수열의 합 : a1,an을 알때 -> n(a1+an)/2
+#               공차, a1알때 -> n{2a + (n-1)d}/2
 n = int(input())
-sum = 0
-for i in range(1,n):
-    sum += i
-print(f'{sum}\n2')
+print(f'{int((n-1)*n/2)}\n2') # (n-1)*n 은 무조건 짝수
