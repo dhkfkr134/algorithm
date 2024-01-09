f3d69bfa1c931241f15836d4b1fe8271f65ee044
김정원 <dhkfkr134@naver.com>
2024-01-09 16:35:16 +0900

    [Silver IV] Title: 설탕 배달, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,13 +1,14 @@
-import math
-check = 0
 N = int(input())
-for i in range(math.ceil(N / 5),-1,-1):
-    for j in range(math.ceil(N/3)+1):
-        if 5*i + 3*j == N:
-            print(i+j)
-            check += 1
-            break
-    if check != 0:
+cnt =0 
+while N >=0:
+    if N % 5 == 0:
+        cnt+=(N//5)
+        N -= (N//5)*5
+    else:
+        N -= 3
+        cnt+=1
+    if N == 0 :
+        print(cnt)
         break
-if check == 0:
+if N < 0 :
     print(-1)
