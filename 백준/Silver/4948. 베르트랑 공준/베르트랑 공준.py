1797981415779aa805082d765576bd9fada925ca
김정원 <dhkfkr134@naver.com>
2024-01-18 17:17:32 +0900

    [Silver II] Title: 베르트랑 공준, Time: 540 ms, Memory: 33048 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,20 +1,21 @@
-num = 123456*2+1
-num_list = [1]*num
-for i in range(1, num):
-    if i == 1:
-        continue
-    for j in range(2, int(i**0.5)+1):
-        if i % j == 0:
-            num_list[i] = 0
-            break
+import sys
+lines = sys.stdin.readlines()
+lines.pop()
+higher = 0
 
-while True:
-    n = int(input())
-    
-    if n == 0:
-        break
-    
-    prime = 0
-    for i in range(n+1, 2*n+1):
-            prime += num_list[i]
-    print(prime)
+for line in lines:
+    higher = max(higher, int(line))
+higher = (higher*2)+1
+isprime = [1]*higher
+for i in range(2,int(higher**0.5)+1):
+    if isprime[i]:
+        for j in range(i*2,higher,i):
+            isprime[j] = 0
+
+for line in lines:
+    n = int(line)
+    count = 0
+    for i in range(n+1,2*n+1):
+        if isprime[i]:
+            count += 1
+    print(count)
