bd72c074e3647bb14e54048078538a632250897c
김정원 <dhkfkr134@naver.com>
2024-02-01 18:06:55 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 176 ms, Memory: 18660 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    
+    LinkedList<Integer> q = new LinkedList<>();
+    int pos = 0;
+    String result ="<";
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+    
+    for(int i = 0; i < N; i++ ) {
+      q.add(i+1);
+    }
+    for(int i=0;i < N-1; i++) {
+      pos = (pos+K-1) % (q.size());
+      result = result + q.remove(pos)+", ";
+    }
+    result = result + q.getFirst()+">";
+    System.out.println(result);
+  }
+}
